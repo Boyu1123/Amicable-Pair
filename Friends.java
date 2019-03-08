7e55becc52567d19017b02f200a8666c0ca18825
Boyu <46700615+Boyu1123@users.noreply.github.com>
2019-03-08 02:35:35 -0800

    Update Friends.java

--- Friends.java
+++ Friends.java
@@ -1,29 +1,17 @@
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  * File Name: Friends.java
- *
- * @author Jagadeesh Vasudevamurthy
+ * @author Boyu Lu
  * @year 2019
- *
- * To compile: RandomInt.java IntUtil.java Friends.java
  */
 public class Friends{
     private int max ;
-    static IntUtil u = new IntUtil() ;
 
-    /*
-     * Constructor
-     */
     Friends(int n) {
         max = n ;
         alg();
     }
 
-    /*
-     *
-     */
     private void alg() {
         int friendNumbers[] = new int[max+1];
         for (int i = 1; i < max; i++) {
@@ -43,21 +31,13 @@ public class Friends{
     }
 
     private static void test() {
-        //int n = 10000;
         int n =  100000000 ;
-        long startTime = System.nanoTime();
         Friends a = new Friends(n) ;
-        long endTime = System.nanoTime();
-        double d2 = u.timeInSec(endTime,startTime) ;
-        System.out.println("Run time r n = " + a.max + " is " + d2 + " secs" ) ;
     }
 
     public static void main(String[] args) {
         System.out.println("Friends.java starts") ;
         test();
-        System.out.println("If you can do in less than 20 secs, you will get FULL points") ;
-        System.out.println("Attach Friends.java, output of your program as a pdf file") ;
-        System.out.println("Attach a word file, explaining why your method is fast") ;
         System.out.println("Friends.java ends") ;
     }
 }
